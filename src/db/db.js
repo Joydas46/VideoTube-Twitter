5c3732054e7224f2e8708c36f922098b5d97ad03
Joydas46 <arkajoy.das@gmail.com>
2024-04-17 08:30:18 +0530

    Updating spacing in db.js

--- src/db/db.js
+++ src/db/db.js
@@ -2,17 +2,17 @@ import mongoose from "mongoose";
 import { DB_Name } from "../constant.js";
 
 const connectDB = async () => {
-  try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGO_DB_URL}${DB_Name}`
-    );
-    console.log(
-      `MongoDB connected !! DB Host: ${connectionInstance.connection.host}`
-    );
-  } catch (error) {
-    console.log("MONGODB CONNECTION ERROR: ", error);
-    process.exit(1);
-  }
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGO_DB_URL}${DB_Name}`
+        );
+        console.log(
+            `MongoDB connected !! DB Host: ${connectionInstance.connection.host}`
+        );
+    } catch (error) {
+        console.log("MONGODB CONNECTION ERROR: ", error);
+        process.exit(1);
+    }
 };
 
 export default connectDB;
