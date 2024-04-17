0a863902d38d4d425ade7e226ef40ed64c71502e
Joydas46 <arkajoy.das@gmail.com>
2024-04-17 08:52:40 +0530

    Add spacing in the async handler util

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,10 @@
 // way-1
 const asyncHandler = (requestHandler) => {
-  return (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => {
-      next(err);
-    });
-  };
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => {
+            next(err);
+        });
+    };
 };
 
 export { asyncHandler };
